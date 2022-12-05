d32961e658b35e0776ac663a86c616ea202cf4d9
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-12-05 10:38:05 -0800

    load todos on page load and when form submitted!

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
-import { createTodo, getUser } from './fetch-utils.js';
+import { createTodo, fetchTodos, getUser } from './fetch-utils.js';
+import { renderTodo } from './render-utils.js';
 
 /* Get DOM Elements */
 const todoForm = document.getElementById('todo-form');
 const errorDisplay = document.getElementById('error-display');
+const todoList = document.getElementById('todo-list');
 
 /* State */
 
 let error = null;
+// let todos = [];
 
 /* Events */
 
@@ -30,7 +33,8 @@ todoForm.addEventListener('submit', async (e) => {
     if (error) {
         displayError();
     } else {
-        return;
+        todoForm.reset();
+        loadTodos();
     }
 });
 
@@ -44,3 +48,15 @@ function displayError() {
         errorDisplay.textContent = '';
     }
 }
+
+async function loadTodos() {
+    todoList.innerHTML = '';
+    const todos = await fetchTodos();
+    for (let todo of todos) {
+        const todoLI = document.createElement('li');
+        todoLI.textContent = todo.detail;
+        todoList.append(todoLI);
+    }
+}
+
+loadTodos();
